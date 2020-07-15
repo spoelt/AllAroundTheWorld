92812b06b6aec45b1cf7ba1e52a494da5fa61d07
suppenkasperl <wi17b015@technikum-wien.at>
2020-07-15 20:50:25 +0200

    add handling for item click in LocationListFragment

--- app/src/main/java/com/spoelt/allaroundtheworld/ui/fragments/LocationListFragment.kt
+++ app/src/main/java/com/spoelt/allaroundtheworld/ui/fragments/LocationListFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -37,9 +38,12 @@ class LocationListFragment : Fragment() {
 
     private val onItemClickListener = object : LocationListAdapter.OnItemClickListener {
         override fun onItemClick(view: View, position: Int) {
-            /*val action = LocationListFragmentDirections.actionLocationListFragmentToLocationDetailFragment()
-            findNavController().navigate(action)*/
-            // tbd
+            val detailLocation = locationAdapter.getLocationAtPosition(position)
+            val action =
+                LocationListFragmentDirections.actionLocationListFragmentToLocationDetailFragment(
+                    detailLocation
+                )
+            findNavController().navigate(action)
         }
     }
 
